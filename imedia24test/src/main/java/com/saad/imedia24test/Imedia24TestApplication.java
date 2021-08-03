2bf5b24c43952faeca53db127d6280ab9b6a9d08
Saad Ayouj <sayouj@pegase.expert>
2021-08-03 16:47:31 +0100

    first commit

---
+++ imedia24test/src/main/java/com/saad/imedia24test/Imedia24TestApplication.java
@@ -0,0 +1,13 @@
+package com.saad.imedia24test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Imedia24TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Imedia24TestApplication.class, args);
+	}
+
+}
---
+++ imedia24test/src/test/java/com/saad/imedia24test/Imedia24TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saad.imedia24test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Imedia24TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
